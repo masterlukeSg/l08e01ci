6a8280c98c1edfc43a1d34b07f4d15793b21a558
Lukas Baumeister <s05lbaum@uni-bonn.de>
2024-07-04 10:49:39 +0200

    Test sollte jtz klappen

--- src/main/java/de/tum/cit/aet/HelloWorldApplication.java
+++ src/main/java/de/tum/cit/aet/HelloWorldApplication.java
@@ -11,7 +11,7 @@ public class HelloWorldApplication {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello World";
+		return "Hello Applied Software Engineering";
 	}
 
 	public static void main(String[] args) {
